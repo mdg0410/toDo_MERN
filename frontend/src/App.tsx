b3cc90b873980fcf75cf8d1a941410f9586b66e4
mdg0410 <levoyersteven@gmail.com>
2025-03-20 15:29:33 -0500

    Agregar layout protegido para rutas; refactorizar la estructura de rutas y mejorar la navegación en la aplicación

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
@@ -8,6 +8,18 @@ import ProtectedRoute from './components/ProtectedRoute';
 import Navbar from './components/Navbar';
 import { verifyUser } from './redux/authSlice';
 import { AppDispatch } from './redux/store';
+
+// Componente que envuelve el layout para rutas protegidas
+const ProtectedLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="pt-4">
+        <Outlet />
+      </div>
+    </>
+  );
+};
 
 function App() {
   const dispatch = useDispatch<AppDispatch>();
@@ -26,16 +38,9 @@ function App() {
         
         {/* Rutas protegidas */}
         <Route element={<ProtectedRoute />}>
-          {/* Navbar aparece en todas las rutas protegidas */}
-          <Route element={<>
-            <Navbar />
-            <div className="pt-4">
-              <Routes>
-                <Route path="/tasks" element={<TaskPage />} />
-              </Routes>
-            </div>
-          </>}>
-            <Route path="/tasks" element={null} />
+          <Route element={<ProtectedLayout />}>
+            <Route path="/tasks" element={<TaskPage />} />
+            <Route path="/" element={<Navigate to="/tasks" replace />} />
           </Route>
         </Route>
         
